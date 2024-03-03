bc6c1f1ddf173a4e12abc8c69d89310a8d829494
martiliones <hi@martilion.es>
2024-03-04 02:00:53 +0500

    fix(CurrencyApi): update API url

--- src/rates/api/currencyapi.ts
+++ src/rates/api/currencyapi.ts
@@ -14,7 +14,7 @@ export interface CurrencyApiDto {
 }
 
 const url =
-  'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json';
+  'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json';
 
 const skipCoins = ['USD', 'BTC', 'ETH'];
 
