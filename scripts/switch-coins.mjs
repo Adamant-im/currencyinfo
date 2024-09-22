49f004e78a6d106623586205b1192473da03259c
yoxira <m71262704@gmail.com>
2024-09-22 11:15:24 +0500

    fix: create fix script for migration script

---
+++ scripts/switch-coins.mjs
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+migrate();
+
+async function migrate() {
+  try {
+    const url = process.argv[2];
+
+    await mongoose.connect(url);
+
+    console.log(
+      `Database '${url}' connected successfully. Starting in 3 seconds...`,
+    );
+
+    await new Promise((resolve) => setTimeout(() => resolve(), 3000));
+
+    const { db } = mongoose.connection;
+
+    const tickers = db.collection('tickers');
+
+    console.log('Starting switching coins...');
+
+    const cursor = timestamps.find({ date: { $lte: 1723404948930 } });
+
+    for await (const doc of cursor) {
+      processedDocs += 1;
+
+      console.log(`Processing ${processedDocs} document: ${doc._id}`);
+
+      const originalBase = doc.base;
+      const originalQuote = doc.quote;
+
+      await tickers.updateOne(
+        { _id: doc._id },
+        {
+          $set: {
+            base: originalQuote,
+            quote: originalBase,
+          },
+        },
+      );
+    }
+
+    console.log('Switching successfully completed.');
+  } catch (error) {
+    console.error('Error during switching coins:', error);
+  } finally {
+    await mongoose.disconnect();
+  }
+}
